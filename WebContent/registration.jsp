f3233899fe69c89cb6b83a00d939391d554a779d
Prince Singh <princeku094@gmail.com>
2021-01-16 20:42:59 +0530

    added two file
    UserReg.java
    register.jsp

---
+++ WebContent/registration.jsp
@@ -0,0 +1,41 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>User Registration</title>
+</head>
+<body>
+<h3>Registration Using Spring</h3>
+
+<table>
+<tr>
+<td><label>Full Name</label></td>
+<td><input type="text" name="uname"></td>
+
+</tr>
+<tr>
+<td><label>Email Id</label></td>
+<td><input type="text" name="uemail"></td>
+
+</tr>
+<tr>
+<td><label>Mobile</label></td>
+<td><input type="text" name="umobile"></td>
+
+</tr>
+<tr>
+<td><label>Job Profile</label></td>
+<td><input type="text" name="ujobprofile"></td>
+
+</tr>
+<tr>
+
+<td><input type="submit" value="Register"></td>
+
+</tr>
+
+</table>
+</body>
+</html>
---
+++ src/com/spring/webProject/UserReg.java
@@ -0,0 +1,39 @@
+package com.spring.webProject;
+
+public class UserReg {
+	
+	int uid;
+	String uname,uemail,umobile,ujobprofile;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUemail() {
+		return uemail;
+	}
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+	public String getUmobile() {
+		return umobile;
+	}
+	public void setUmobile(String umobile) {
+		this.umobile = umobile;
+	}
+	public String getUjobprofile() {
+		return ujobprofile;
+	}
+	public void setUjobprofile(String ujobprofile) {
+		this.ujobprofile = ujobprofile;
+	}
+	
+	
+}
